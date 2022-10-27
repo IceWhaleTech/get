a0c83733f2f415941210aa691ab45c7cb1fc0a02
Tiger Wang <tigerwang@outlook.com>
2022-10-26 22:37:20 -0400

    wip

--- update.sh
+++ update.sh
@@ -201,9 +201,9 @@ Check_Arch() {
     CASA_PACKAGES=(
         "https://github.com/IceWhaleTech/CasaOS-Gateway/releases/download/v0.3.6/linux-${TARGET_ARCH}-casaos-gateway-v0.3.6.tar.gz"
         "https://github.com/IceWhaleTech/CasaOS-UserService/releases/download/v0.3.7/linux-${TARGET_ARCH}-casaos-user-service-v0.3.7.tar.gz"
-        "https://github.com/IceWhaleTech/CasaOS-LocalStorage/releases/download/v0.3.7-alpha9/linux-${TARGET_ARCH}-casaos-local-storage-v0.3.7-alpha9.tar.gz"
-        "https://github.com/IceWhaleTech/CasaOS/releases/download/v0.3.7-alpha7/linux-${TARGET_ARCH}-casaos-v0.3.7-alpha7.tar.gz"
-        "https://github.com/IceWhaleTech/CasaOS-UI/releases/download/v0.3.7-alpha/linux-all-casaos-v0.3.7-alpha.tar.gz"
+        "https://github.com/IceWhaleTech/CasaOS-LocalStorage/releases/download/v0.3.7/linux-${TARGET_ARCH}-casaos-local-storage-v0.3.7.tar.gz"
+        "https://github.com/IceWhaleTech/CasaOS/releases/download/v0.3.7/linux-${TARGET_ARCH}-casaos-v0.3.7.tar.gz"
+        "https://github.com/IceWhaleTech/CasaOS-UI/releases/download/v0.3.7/linux-all-casaos-v0.3.7.tar.gz"
     )
 }
 
