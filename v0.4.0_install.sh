ee73384bb487f59a3417f7c97e7c363ede16fd51
老竭力 <jerrykuku@qq.com>
2022-12-21 22:23:55 +0800

    Update v0.4.0_install.sh

--- v0.4.0_install.sh
+++ v0.4.0_install.sh
@@ -183,17 +183,14 @@ exist_file() {
 # 0 Get download url domain
 # To solve the problem that Chinese users cannot access github.
 Get_Download_Url_Domain() {
-    Show 0 "Start to check region"
-    # Use https://api.myip.la/en and https://ifconfig.io/country_code to get the country code
-    #REGION=$(${sudo_cmd} curl --connect-timeout 2 -s https://api.myip.la/en | awk '{print $2}')
-    REGION=$(${sudo_cmd} curl --connect-timeout 0.1 -s ipconfig.io/country)
-    #if [ "${REGION}" = "" ]; then
-    #    REGION=$(${sudo_cmd} curl --connect-timeout 2 -s https://ifconfig.io/country_code)
-    #fi
-    if [[ "${REGION}" = "China" ]]; then
+    # Use ipconfig.io/country and https://ifconfig.io/country_code to get the country code
+    REGION=$(${sudo_cmd} curl --connect-timeout 2 -s ipconfig.io/country || echo "")
+    if [ "${REGION}" = "" ]; then
+       REGION=$(${sudo_cmd} curl --connect-timeout 2 -s https://ifconfig.io/country_code || echo "")
+    fi
+    if [[ "${REGION}" = "China" ]] || [[ "${REGION}" = "CN" ]]; then
         CASA_DOWNLOAD_DOMAIN="https://casaos.oss-cn-shanghai.aliyuncs.com/"
     fi
-    Show 0 "Check region OK"
 }
 
 
